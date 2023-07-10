ecb3d5376cb886e69b72f32f4fb4e533fc1ef19f
jessica <jessicajoseph2019@yahoo.com>
2023-07-10 02:15:39 +0100

    feat: testimonials & CTA

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
